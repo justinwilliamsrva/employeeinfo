030aae42cd498f752a1716a13a64b364b03eecc1
justinwilliamsrva <justin.williams2@me.com>
2020-08-11 13:53:13 -0400

    final draft

--- app.js
+++ app.js
@@ -232,16 +232,17 @@ function createteam() {
             '<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"/>' +
             '<meta name="viewport" content="width=device-width, initial-scale=1.0"/> ' +
             '<meta http-equiv="X-UA-Compatible" content="ie=edge" />' +
+            "<title>Company Team Page</title>" +
             "</head>" +
             "<body>" +
             "<header>" +
-            "<h1>" +
+            '<h1 class="text-center">' +
             "Company Team Page" +
             "</h1>" +
             "</header>" +
             `<div class="container"` +
             '<div class="row">' +
-            '<div class="col-sm-10">'
+            '<div class="col-12">'
     );
 
     fs.appendFileSync(
